c6fbeca7d4bb3d50105b2ec062e95c51831e4f26
chavi-i12 <71093852+chavi-i12@users.noreply.github.com>
2020-11-02 09:47:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var rect1, rect2, rect3;
+var paperObject;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 600);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.\
+
+	var groundOptions = {
+		isStatic: true
+	}
+	groundSprite=Bodies.rectangle(width/2, height-65, width,10, groundOptions);
+	World.add(world,groundSprite);
+
+    var rectOptions = {
+		isStatic: true
+	}
+	rectSprite1 = Bodies.rectangle(750,480,20,100,rectOptions);
+	rectSprite2 = Bodies.rectangle(950,480,20,100,rectOptions);
+	rectSprite3 = Bodies.rectangle(850,520,220,20,rectOptions);
+
+	var options={
+		isStatic:false,
+		restitution:0.2,
+		friction:0.3,
+		density:1.4
+	}
+	paperObject = Bodies.circle(80,450,20,options);
+	World.add(world,paperObject);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  fill("yellow");
+  rectMode(CENTER);
+  rect(groundSprite.position.x,groundSprite.position.y,width,10);
+  rect(rectSprite1.position.x, rectSprite1.position.y,20,100);
+  rect(rectSprite2.position.x, rectSprite2.position.y,20,100);
+  rect(rectSprite3.position.x, rectSprite3.position.y,220,20);
+
+ ellipseMode(RADIUS);
+ ellipse(paperObject.position.x,paperObject.position.y,20,20);
+  
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+		Matter.Body.applyForce(paperObject,paperObject.position,{x:85,y:-85});
+	}
+}
+
+
+
